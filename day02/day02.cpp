b1670fa15ecee1005247f51854b57579731201fd
Thomas Hooper <tom@stainlessvision.com>
2024-12-17 19:53:34 +0000

    Day 2 Part 2

--- day02/day02.cpp
+++ day02/day02.cpp
@@ -35,6 +35,69 @@ bool reportIsSafe(std::string line)
     return true;
 }
 
+bool reportIsSafeWithSafeguards(std::string line)
+{
+    std::stringstream linestream{};
+    int lastValue{ 0 };
+    int value{ 0 };
+    int index{ 0 };
+    int loopindex{ 0 };
+    bool increasing{ false };
+    bool lastIncreasing{ false };
+    int skipIndex{ -1 };
+
+    while(true)
+    {
+        linestream.str("");
+        linestream.clear();
+        linestream << line;
+
+        loopindex = -1;
+        index = 0;
+
+        while (linestream >> value)
+        {
+            loopindex++;
+            if (loopindex == skipIndex)
+            {
+                continue;
+            }
+            if (index > 0)
+            {
+                if (abs(value - lastValue) > 3 || value == lastValue)
+                {
+                    goto nextPermutation;
+                }
+                increasing = value > lastValue;
+            }
+            if (index > 1 && increasing != lastIncreasing)
+            {
+                goto nextPermutation;
+            }
+            index++;
+            lastValue = value;
+            lastIncreasing = increasing;
+        }
+
+        return true;
+
+        nextPermutation:;
+        if (skipIndex > -1)
+        {
+            if (skipIndex == 0)
+            {
+                return false;
+            }
+            skipIndex--;
+        }
+        else
+        {
+            skipIndex = index;
+        }
+    }
+}
+
+
 int main()
 {
     std::ifstream input{ "input.txt" };
@@ -48,12 +111,29 @@ int main()
     int safeReports{ 0 };
     std::string line{};
 
+
     while (std::getline(input, line))
     {
-        if (reportIsSafe(line)) {
+        if (reportIsSafe(line))
+        {
             safeReports++;
         }
     }
 
-    std::cout << safeReports << '\n';
+    std::cout << "Safe reports: " << safeReports << '\n';
+
+    input.clear();
+    input.seekg(0);
+
+    safeReports = 0;
+
+    while (std::getline(input, line))
+    {
+        if (reportIsSafeWithSafeguards(line))
+        {
+            safeReports++;
+        }
+    }
+
+    std::cout << "Safe reports with safeguards: " << safeReports << '\n';
 }
