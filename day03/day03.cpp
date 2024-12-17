be31064a6c1060f597901fde7a1e93a6ef731acc
Thomas Hooper <tom@stainlessvision.com>
2024-12-17 21:12:58 +0000

    Day 3 Part 2

--- day03/day03.cpp
+++ day03/day03.cpp
@@ -18,16 +18,46 @@ int main()
     inputstream << input.rdbuf();
     std::string inputstring = inputstream.str();
 
-    std::regex pattern("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)");
-    auto begin = std::sregex_iterator(inputstring.begin(), inputstring.end(), pattern);
-    auto end = std::sregex_iterator();
+    std::regex mulPattern("mul\\(([0-9]{1,3}),([0-9]{1,3})\\)");
+    auto mulBegin = std::sregex_iterator(inputstring.begin(), inputstring.end(), mulPattern);
+    auto mulEnd = std::sregex_iterator();
 
     int result{ 0 };
 
-    for (std::sregex_iterator i = begin; i != end; ++i)
+    for (std::sregex_iterator i = mulBegin; i != mulEnd; ++i)
     {
         std::smatch match = *i;
         result += std::stoi(match[1].str()) * std::stoi(match[2].str());
+    }
+
+    std::cout << result << '\n';
+
+    result = 0;
+
+    std::regex doPattern("do(n't)?\\(\\)");
+    auto doBegin = std::sregex_iterator(inputstring.begin(), inputstring.end(), doPattern);
+    auto doEnd = std::sregex_iterator();
+    std::sregex_iterator doIterator = doBegin;
+    
+    bool enabled = true;
+    for (std::sregex_iterator i = mulBegin; i != mulEnd; ++i)
+    {
+        std::smatch mulMatch = *i;
+        int mulIndex = mulMatch.position();
+
+        while (doIterator != doEnd) {
+            std::smatch doMatch = *doIterator;
+            int doIndex = doMatch.position();
+            if (doIndex > mulIndex) {
+                break;
+            }
+            enabled = doMatch[1].str().empty();
+            doIterator++;
+        }
+
+        if (enabled) {
+            result += std::stoi(mulMatch[1].str()) * std::stoi(mulMatch[2].str());
+        }
     }
 
     std::cout << result << '\n';
